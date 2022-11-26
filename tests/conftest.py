1e793f967a36ddabf0dcdccd321d001aa61534fb
alena-p <sunny208@yandex.ru>
2022-11-26 19:44:15 +0300

    add allure decorators

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
+import allure
 import pytest
 from selene.support.shared import browser
 
+with allure.step('Configure browser'):
 
-@pytest.fixture(scope="function", autouse=True)
-def browser_config():
-    browser.config.base_url = "https://demoqa.com"
-    browser.config.browser_name = "chrome"
+    @pytest.fixture(scope="function", autouse=True)
+    def browser_config():
+        browser.config.base_url = "https://demoqa.com"
+        browser.config.browser_name = "chrome"
 
-    yield
+        yield

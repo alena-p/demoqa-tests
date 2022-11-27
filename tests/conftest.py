80055ed4850c92f8433dcd36c48319a50aa0f6af
alena-p <sunny208@yandex.ru>
2022-11-27 15:34:38 +0300

    add config for remote browser

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import allure
 import pytest
 from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from demoqa_test.utils import attachments
 
@@ -8,8 +10,24 @@ with allure.step('Configure browser'):
 
     @pytest.fixture(scope="function", autouse=True)
     def browser_config():
+        options = Options()
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": "100.0",
+            "selenoid:options": {"enableVNC": True, "enableVideo": True},
+        }
+
+        options.capabilities.update(selenoid_capabilities)
+
+        driver = webdriver.Remote(
+            command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+            options=options,
+        )
+
+        browser.config.driver = driver
         browser.config.base_url = "https://demoqa.com"
-        browser.config.browser_name = "chrome"
+        browser.config.window_width = 1920
+        browser.config.window_height = 900
 
         yield browser
 
